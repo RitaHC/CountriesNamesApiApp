1e5961195054916a69d7b5fd9ef77f4175193c21
Rita HC <ritahameerchauhan@gmail.com>
2023-05-14 22:21:10 -0400

    Countried Data listening on Port 8000

--- main.go
+++ main.go
@@ -2,70 +2,45 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"log"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
-type Movies struct {
-	Movies []Movie `json:"movies"`
-}
-
-type Movie struct {
-	Title    string `json:"title"`
-	Year     int    `json:"year"`
-	Imdb_id  string `json:"imdb_id"`
-	Genre    string `json:"genre"`
-	Language string `json:"language"`
-	Size     int    `json:"size"`
-	Quality  string `json:"quality"`
-	Seeds    int    `json:"seeds"`
-	Leeches  int    `json:"leeches"`
+type Country struct {
+	Name struct {
+		Common   string `json:"common"`
+		Official string `json:"official"`
+	} `json:"name"`
 }
 
 func main() {
-	// Create a new gin.Engine object
-	r := gin.Default()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			url := "https://restcountries.com/v3.1/all"
 
-	// Add your routes here
-	r.GET("/movies", func(c *gin.Context) {
-		// This line defines a new route that handles GET requests to the `/movies` path.
+			response, err := http.Get(url)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		// Make a request to the YTS API
-		response, err := http.Get("https://yts.mx/api/v2/list_movies.json")
-		if err != nil {
-			c.JSON(500, gin.H{
-				"message": err.Error(),
-			})
-			return
+			defer response.Body.Close()
+
+			var countries []Country
+
+			err = json.NewDecoder(response.Body).Decode(&countries)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(countries)
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			w.Write([]byte("405 - Method Not Allowed"))
 		}
-
-		// Read the response body
-		body, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			c.JSON(500, gin.H{
-				"message": err.Error(),
-			})
-			return
-		}
-
-		// Parse the JSON response
-		var movies Movies
-		err = json.Unmarshal(body, &movies)
-		if err != nil {
-			c.JSON(500, gin.H{
-				"message": err.Error(),
-			})
-			return
-		}
-
-		// Send the movies to the client
-		c.JSON(200, movies)
 	})
 
-	// Start the server
-	http.ListenAndServe(":8000", r)
+	log.Println("Listening on :8000...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
-
 
